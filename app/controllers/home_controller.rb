77ae8f77c4952e153c4e61fa835b2f09c4dc816d
Denis Erofeev <edenisn@gmail.com>
2017-06-29 02:01:00 +0500

    Add search feature to repo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @books = Book.paginate(page: params[:page], per_page: 10)
+    @books = if params[:search]
+               Book.search(params[:search]).order("created_at DESC").paginate(page: params[:page], per_page: 3)
+             else
+               Book.paginate(page: params[:page], per_page: 10)
+             end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,8 @@ class Book < ActiveRecord::Base
 
   validates :isbn, uniqueness: true
   validates :name, :isbn, presence: true
+
+  def self.search(query)
+    where('name LIKE :search OR isbn LIKE :search', search: "%#{query}%")
+  end
 end
