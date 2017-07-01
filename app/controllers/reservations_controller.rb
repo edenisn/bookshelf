807c78b4c75bdb902e525d95e55cadac404e601e
Denis Erofeev <edenisn@gmail.com>
2017-07-01 20:00:38 +0500

    Add more syntax sugar

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,26 +2,25 @@ class ReservationsController < ApplicationController
   before_action :authenticate_user!
 
   def new
-    find_book
-
-    if @book.present?
-      @book.update_attributes(user_id: current_user.id, reserved: true)
-      redirect_to books_path
-    end
+    update_book_attributes(current_user.id, true)
   end
 
   def destroy
-    find_book
-
-    if @book.present?
-      @book.update_attributes(user_id: nil, reserved: false)
-      redirect_to books_path
-    end
+    update_book_attributes
   end
 
   private
 
   def find_book
     @book = Book.find(params[:id])
+  end
+
+  def update_book_attributes(user_id = nil, reserved = false)
+    find_book
+
+    if @book.present?
+      @book.update_attributes(user_id: user_id, reserved: reserved)
+      redirect_to books_path
+    end
   end
 end
